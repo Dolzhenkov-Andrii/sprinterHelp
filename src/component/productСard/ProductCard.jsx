5c2b14b7d0df344220d242742548bdb731183d6d
Dolzhenkov-Andrii <oscurabow@gmail.com>
2024-03-12 00:56:46 +0200

    Create component "Product Card"

--- src/component/productСard/ProductCard.jsx
+++ src/component/productСard/ProductCard.jsx
@@ -7,29 +7,31 @@ import img1 from "/img/img1.jpg"
 
 export default function ProductCard({ className }) {
 
-    const isUsedIndex = true;
+    const isUsedIndex = false;
 
     return (
-        <div className={`${classes.backgroundBox} ${isUsedIndex ? classes.usedIndex : ''} ${className}`}>
-            <div className={classes.imgBox}>
-                <div className={`${classes.usedIndexBox} ${classes.notActive}`}><p>{"Б/В"}</p></div>
-                {/* <img className={classes.img} src={img1} alt="Product" /> */}
-            </div>
-            <div className={classes.infoBox}>
-                <div content={classes.textInfo}>
-                    <p className={classes.title}>{"title"}</p>
-                    <hr className={classes.infoTextLine} />
-                    <p className={classes.text}>{"text"}</p>
+        <div className={classes.containerBoxShadow}>
+            <div className={`${classes.backgroundBox} ${isUsedIndex ? classes.usedIndex : ''} ${className} `}>
+                <div className={`${classes.imgBox} ${isUsedIndex ? classes.usedIndexImg : ''}`}>
+                    <div className={`${classes.usedIndexBox} ${isUsedIndex ? '' : classes.notActive}`}><p>{"Б/В"}</p></div>
+                    <img className={classes.img} src={img1} alt="Product" />
                 </div>
-                <div className={classes.priceInfo}>
-                    <div className={classes.priceBox}>
-                        <p className={classes.newPrice}>{"2000 грн"}</p>
-                        <p className={classes.oldPrice}>{"3000 грн"}</p>
+                <div className={classes.infoBox}>
+                    <div content={classes.textInfo}>
+                        <p className={classes.title}>{"title"}</p>
+                        <hr className={classes.infoTextLine} />
+                        <p className={classes.modelAuto}>{"modelAuto"}</p>
                     </div>
-                    <Button>
-                        <p>+</p>
-                        <SvgIcons name={"basket"} className={""} />
-                    </Button>
+                    <div className={classes.priceInfo}>
+                        <div className={classes.priceBox}>
+                            <p className={classes.newPrice}>{"2000 грн"}</p>
+                            <p className={classes.oldPrice}>{"3000 грн"}</p>
+                        </div>
+                        <Button className={classes.basketButton}>
+                            <p>+</p>
+                            <SvgIcons name={"basket"} className={classes.basketSvgIcons} />
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
